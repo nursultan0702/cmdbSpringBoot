ddd61e698e6792defbcc1f351c68d90ad5f40f4b
Nursultan <nursultan.sadyk1995@gmail.com>
2017-11-21 15:13:29 +0600

    changed graphs from predix

---
+++ src/main/java/com/example/demo/Repositories/TurbineDataRepository.java
@@ -0,0 +1,10 @@
+package com.example.demo.Repositories;
+
+import com.example.demo.Entities.TurbineData;
+import org.springframework.data.repository.CrudRepository;
+
+/**
+ * Created by Disney on 09.11.2017.
+ */
+public interface TurbineDataRepository extends CrudRepository<TurbineData, Long> {
+}
